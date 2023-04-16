81c3ee57e33f85a0bafe865d9c4f9b53811800b8
igor konst <uropb.8@mail.ru>
2023-04-16 23:24:22 +0300

    layout,leaderboard

--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -4,28 +4,30 @@ import Link from "next/link";
 
 const Footer = ({ ...props }) => {
   return (
-    <footer {...props}>
-      <div className={styles.btn}>
-        <Button>Join Newsletter</Button>
-      </div>
-      <ul className={styles.nav}>
-        <li>
-          <Link href="/" className={styles.nav__link}>
-            Discord
-          </Link>
-        </li>
-        <li>
-          <Link href="/" className={styles.nav__link}>
-            Twitter
-          </Link>
-        </li>
-        <li>
-          <Link href="/" className={styles.nav__link}>
-            Contact Us
-          </Link>
-        </li>
-      </ul>
-    </footer>
+    <div {...props}>
+      <footer className={styles.footer}>
+        <div className={styles.btn}>
+          <Button>join</Button>
+        </div>
+        <ul className={styles.nav}>
+          <li>
+            <Link href="/" className={styles.nav__link}>
+              Discord
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className={styles.nav__link}>
+              Twitter
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className={styles.nav__link}>
+              Contact Us
+            </Link>
+          </li>
+        </ul>
+      </footer>
+    </div>
   );
 };
 
