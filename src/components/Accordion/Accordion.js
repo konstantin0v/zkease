e8c121fa5d168a28af4f4b1a69dbc18e5218ef26
Igor Konst <igorkonst00@gmail.com>
2023-05-09 22:29:16 +0300

    start

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -1,37 +1,27 @@
-import { useEffect, useState } from "react";
-import styles from "./Accordion.module.css";
-import { motion } from "framer-motion";
-import { CloserSvg, GuideSvg, OpenerSvg } from "@/components";
-import clsx from "clsx";
+import { useState } from 'react';
+import styles from './Accordion.module.css';
+import { CloserSvg, GuideSvg, OpenerSvg } from '@/components';
+import clsx from 'clsx';
 
 const Accordion = ({ content }) => {
   const [isActive, setIsActive] = useState(false);
-  let notFirstRednder = false;
-
-  useEffect(() =>{
-    if(isActive === true) notFirstRednder = true;
-  }, [isActive])
 
   return (
-    <>
+    <div className={clsx(styles.accord, isActive && styles.accord__active)}>
       <div
         className={clsx(styles.title, isActive && styles.title__active)}
-        onClick={() => setIsActive(!isActive)}
+        onClick={() => setIsActive((prev) => !prev)}
       >
-        <div className={styles.top}>
+        <div className={styles.title__left}>
           <GuideSvg />
-          <div className={styles.titleText}>GUIDE</div>
+          <p className={styles.title__text}>Guide</p>
         </div>
         {isActive ? <CloserSvg /> : <OpenerSvg />}
       </div>
-        <motion.div
-          initial={{ y: -20, opacity:0 }}
-          animate={isActive ? { y: 0, opacity:1, transition: { duration: 0.8 }} : { y: -20, opacity:0, transition: { duration: 0.8 }}} 
-          className={styles.accordionContent}
-        >
-          {content}
-        </motion.div>
-    </>
+      <div className={clsx(styles.content, isActive && styles.content__active)}>
+        {content}
+      </div>
+    </div>
   );
 };
 
